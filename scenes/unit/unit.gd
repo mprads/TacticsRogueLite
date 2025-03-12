643f7fa2c7c1ff9e0df034d9c1d57e42548b279e
Myles Rademaker <mylesprademaker@gmail.com>
2025-03-11 18:00:14 -0700

    Added temp files to git ignore, created basic unit scene to test unit stats generation

---
+++ scenes/unit/unit.gd
@@ -0,0 +1,23 @@
+extends Area2D
+class_name Unit
+
+@export var stats: UnitStats : set = set_stats
+
+@onready var outline: Sprite2D = $Visuals/Outline
+@onready var filling: Sprite2D = $Visuals/Filling
+
+
+func set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if value == null:
+		return
+	
+	if not is_node_ready():
+		await ready
+		
+	stats = value
+
+	outline.region_rect.position = Vector2(stats.bottle.sprite_coordinates) * 32
+	filling.region_rect.position = Vector2(stats.bottle.sprite_coordinates) * 32
+	filling.modulate = stats.potion.color
