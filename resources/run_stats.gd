1af93b0a5a25f529c3ef3c66f9c1ac05d481d891
Myles Rademaker <mylesprademaker@gmail.com>
2025-03-19 20:03:29 -0700

    Updated potion resources to make it easier to test and added vial assets

--- resources/run_stats.gd
+++ resources/run_stats.gd
@@ -2,12 +2,15 @@ extends Resource
 class_name RunStats
 
 const STARTING_GOLD := 50
-const STARTING_PARTY_SIZE := 6
+const STARTING_PARTY_SIZE := 5
+const STARTING_VIAL_COUNT := 3
 
 @export var gold := STARTING_GOLD
 @export var inventory: Dictionary[ItemConfig.KEYS, int]
 @export var party: Array[UnitStats]
-@export var party_size := STARTING_PARTY_SIZE
+@export var max_party_size := STARTING_PARTY_SIZE
+@export var vials: Array[Vial]
+@export var max_vial_count := STARTING_VIAL_COUNT
 
 
 func add_item_to_inventory(key: ItemConfig.KEYS) -> void:	
---
+++ resources/vials/vial.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name Vial
+
+@export_category("Visuals")
+@export var outline_16: Texture2D
+@export var fillint_16: Texture2D
+@export var outline_32: Texture2D
+@export var fillint_32: Texture2D
+
+@export var potion: Potion
+@export var quantity := 1
