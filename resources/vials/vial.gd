4ec8043c44424d0f8251576cbd92e7b71c664a03
Myles Rademaker <mylesprademaker@gmail.com>
2025-03-25 13:06:33 -0700

    Updated bottle sprites, added 5 basic potions with names rather than testing colours. Updated vial sprite. Updated all references in item config

--- resources/vials/vial.gd
+++ resources/vials/vial.gd
@@ -2,10 +2,8 @@ extends Resource
 class_name Vial
 
 @export_category("Visuals")
-@export var outline_16: Texture2D
-@export var fillint_16: Texture2D
-@export var outline_32: Texture2D
-@export var fillint_32: Texture2D
+@export var outline: Texture2D
+@export var filling: Texture2D
 
 @export var potion: Potion : set = set_potion
 @export var refill_quantity := 1
