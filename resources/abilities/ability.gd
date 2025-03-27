6918aede6fccdc7856f1154ba94122b3c85d5319
Myles Rademaker <mylesprademaker@gmail.com>
2025-03-26 20:26:35 -0700

    Fixed issue with upsacled tile sheet creating mixels. Added shape attribute on ability to store an array of vector2i for aoe abilities. Made temp absolutely terrible way to store maps, like terrible

--- resources/abilities/ability.gd
+++ resources/abilities/ability.gd
@@ -2,7 +2,7 @@ extends Resource
 class_name Ability
 
 enum TYPE { ATTACK, STATUS }
-enum TARGET { SELF, SINGLE_ALLY, SINGLE_ENEMY, ALL_ENEMIES }
+enum TARGET { SELF, SINGLE_ALLY, SINGLE_ENEMY, ALL_ENEMIES, AOE }
 
 @export_category("Visuals")
 @export var name: String
@@ -11,4 +11,5 @@ enum TARGET { SELF, SINGLE_ALLY, SINGLE_ENEMY, ALL_ENEMIES }
 @export_category("Effects")
 @export var type: TYPE
 @export var target: TARGET
+@export var shape: Array[Vector2i]
 @export var range := 1
