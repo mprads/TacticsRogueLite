bf13f445be433f5b60cb31fc08beeaaf48f44c60
Myles Rademaker <mylesprademaker@gmail.com>
2025-04-15 13:39:46 -0700

    Added range check to abilities

--- scenes/battle/ability_manager.gd
+++ scenes/battle/ability_manager.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name AbilityManager
 
+@export var arena: Arena
+
 var aiming_unit: Unit
 var current_ability: Ability
 
@@ -12,16 +14,21 @@ func has_active_ability() -> bool:
 func handle_unit_aim(unit: Unit, ability: Ability) -> void:
 	aiming_unit = unit
 	current_ability = ability
+	
+	arena.tile_highlighter.enabled = true
 
 
 func handle_aim_stopped() -> void:
 	aiming_unit = null
 	current_ability = null
+	
+	arena.tile_highlighter.enabled = false
 
 
 func handle_selected_unit(target: Unit) -> void:
 	if not current_ability: return
-
+	if not _within_range(target): return
+	
 	match current_ability.target:
 		Ability.TARGET.SELF:
 			if target == aiming_unit:
@@ -33,7 +40,18 @@ func handle_selected_unit(target: Unit) -> void:
 
 func handle_selected_enemy(target: Enemy) -> void:
 	if not current_ability: return
-	
+	if not _within_range(target): return
+
 	if current_ability.target == Ability.TARGET.SINGLE_ENEMY:
 		if target.is_in_group("enemy_unit"):
 			aiming_unit.unit_state_machine.use_ability([target])
+
+
+func _within_range(target: Node) -> bool:
+	var aiming_tile := arena.get_tile_from_global(aiming_unit.global_position)
+	var target_tile := arena.get_hovered_tile()
+	
+	var delta: Vector2i = (target_tile - aiming_tile).abs()
+	var distance := int(delta.x + delta.y)
+	
+	return distance <= current_ability.max_range
