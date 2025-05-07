fc1ac27a46b4275b240775110faadeb15865d605
Myles Rademaker <mylesprademaker@gmail.com>
2025-05-07 13:59:49 -0700

    Updated party select to add the selected items and unit to the lower third of the screen to represent player's choices

--- scenes/party_select/party_select.gd
+++ scenes/party_select/party_select.gd
@@ -1,7 +1,9 @@
 extends Control
 class_name PartySelect
 
-const UNIT_SELECT_PANEL = preload("res://scenes/party_select/unit_select_panel.tscn")
+const UNIT_SELECT_PANEL_SCENE = preload("res://scenes/party_select/unit_select_panel.tscn")
+const UNIT_ICON_PANEL_SCENE = preload("res://scenes/ui/unit_icon_panel.tscn")
+const ITEM_PANEL_SCENE = preload("res://scenes/ui/item_panel.tscn")
 const OPTION_COUNT := 3
 
 @export_category("Pools")
@@ -11,36 +13,57 @@ const OPTION_COUNT := 3
 @export var starter_artifacts: Array[Artifact]
 
 @export_category("Ranges")
+@export var vial_odds: float
 @export var gold_odds: float
 @export var gold_min: int
 @export var gold_max: int
 @export var item_quantity_min: int
 @export var item_quantity_max: int
 
+@export var run_stats: RunStats
+@onready var inventory_manager: InventoryManager = $InventoryManager
+@onready var party_manager: PartyManager = $PartyManager
+@onready var vial_manager: VialManager = $VialManager
+@onready var artifact_manager: ArtifactManager = $ArtifactManager
+
 @onready var selection_container: HBoxContainer = %SelectionContainer
+@onready var party_container: HBoxContainer = %PartyContainer
+@onready var vial_container: HBoxContainer = %VialContainer
+@onready var inventory_container: HBoxContainer = %InventoryContainer
 
 
 func _ready() -> void:
+	if not run_stats:
+		run_stats = RunStats.new()
+		_set_up_managers()
+
+	_set_up_connections()
+	_generate_options()
+
+
+func _set_up_managers() -> void:
+	inventory_manager.run_stats = run_stats
+	party_manager.run_stats = run_stats
+	vial_manager.run_stats = run_stats
+	artifact_manager.run_stats = run_stats
+
+
+func _set_up_connections() -> void:
+	inventory_manager.inventory_changed.connect(_on_inventory_changed)
+	inventory_manager.gold_changed.connect(_on_gold_changed)
+	party_manager.party_changed.connect(_on_party_changed)
+	vial_manager.vials_changed.connect(_on_vials_changed)
+
+
+func _generate_options() -> void:
 	for option in OPTION_COUNT:
-		var unit_select_instance := UNIT_SELECT_PANEL.instantiate()
+		var unit_select_instance := UNIT_SELECT_PANEL_SCENE.instantiate()
 		selection_container.add_child(unit_select_instance)
 		var unit_stats := _generate_unit_stats()
 		unit_select_instance.unit_stats = unit_stats
+		unit_select_instance.panel_selected.connect(_on_panel_selected)
 
-		var item_contents := []
-
-		for item in OPTION_COUNT:
-			var chance := randf_range(0.0, 1.0)
-
-			if chance > gold_odds:
-				var starter_item: Item = RNG.array_pick_random(starter_items)
-				var quantity := RNG.instance.randi_range(item_quantity_min, item_quantity_max)
-				item_contents.append({ "item": starter_item, "quantity": quantity })
-			else:
-				var gold_value := RNG.instance.randi_range(gold_min, gold_max)
-				item_contents.append({ "item": "gold", "quantity": gold_value })
-
-		unit_select_instance.content = item_contents
+		unit_select_instance.contents = _generate_items()
 
 
 func _generate_unit_stats() -> UnitStats:
@@ -51,3 +74,67 @@ func _generate_unit_stats() -> UnitStats:
 	unit_stats.potion = potion
 
 	return unit_stats
+
+
+func _generate_items() -> Array:
+	var item_contents := []
+
+	for item in OPTION_COUNT:
+			var chance := randf_range(0.0, 1.0)
+
+			if chance > gold_odds:
+				var starter_item: Item = RNG.array_pick_random(starter_items)
+				var quantity := RNG.instance.randi_range(item_quantity_min, item_quantity_max)
+				item_contents.append({ "item": starter_item, "quantity": quantity })
+			else:
+				var gold_value := RNG.instance.randi_range(gold_min, gold_max)
+				item_contents.append({ "item": "gold", "quantity": gold_value })
+
+	return item_contents
+
+
+func _on_panel_selected(unit_stats: UnitStats, contents: Array) -> void:
+	party_manager.add_unit(unit_stats)
+	
+	for content in contents:
+		if content.item is Item:
+			inventory_manager.add_item(content.item, content.quantity)
+		elif content.item is Vial:
+			pass
+		elif content is Artifact:
+			pass
+		else:
+			inventory_manager.add_gold(content.quantity)
+
+
+func _on_inventory_changed() -> void:
+	for child in inventory_container.get_children():
+		child.queue_free()
+
+	var inventory := inventory_manager.get_inventory()
+
+	for item in inventory:
+		var item_panel_instance := ITEM_PANEL_SCENE.instantiate()
+		inventory_container.add_child(item_panel_instance)
+		item_panel_instance.item = ItemConfig.get_item_resource(item)
+		item_panel_instance.count = inventory[item]
+
+
+func _on_gold_changed() -> void:
+	pass
+
+
+func _on_party_changed() -> void:
+	for child in party_container.get_children():
+		child.queue_free()
+
+	var party := party_manager.get_party()
+
+	for unit_stats in party:
+		var unit_panel_instance := UNIT_ICON_PANEL_SCENE.instantiate()
+		party_container.add_child(unit_panel_instance)
+		unit_panel_instance.unit = unit_stats
+
+
+func _on_vials_changed() -> void:
+	pass
--- scenes/party_select/unit_select_panel.gd
+++ scenes/party_select/unit_select_panel.gd
@@ -1,21 +1,30 @@
 extends PanelContainer
 class_name UnitSelectPanel
 
+signal panel_selected(unit_stats: UnitStats, items: Array)
+
 const TEST_ROUND_UNIT = preload("res://resources/units/test_round_unit.tres")
 const ABILITY_PANEL_SCENE = preload("res://scenes/ui/ability_panel.tscn")
 const STARTING_ITEM_PANEL_SCENE = preload("res://scenes/party_select/starting_item_panel.tscn")
 const GOLD_ICON = preload("res://assets/icons/gold.png")
 
 @export var unit_stats: UnitStats : set = set_unit_stats
-@export var content: Array : set = set_content
+@export var contents: Array : set = set_contents
 
 @onready var ability_container: HBoxContainer = %AbilityContainer
 @onready var party_unit_ui: PartyUnitUI = %PartyUnitUI
-@onready var contents: VBoxContainer = %Contents
+@onready var contents_container: VBoxContainer = %ContentsContainer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var disabled := false
 
 
 func _ready() -> void:
+	gui_input.connect(_on_gui_input)
+	mouse_entered.connect(_on_mouse_entered)
 	unit_stats = TEST_ROUND_UNIT
+
+	_play_animation("in")
 
 
 func _update_ability_visuals(unit_stats: UnitStats) -> void:
@@ -30,6 +39,14 @@ func _update_ability_visuals(unit_stats: UnitStats) -> void:
 		ability_panel_instance.ability = ability
 
 
+func _play_animation(id: String) -> void:
+	if animation_player.is_playing():
+		animation_player.stop()
+
+	if not disabled:
+		animation_player.play(id)
+
+
 func set_unit_stats(value: UnitStats) -> void:
 	unit_stats = value
 	party_unit_ui.unit = unit_stats
@@ -37,12 +54,12 @@ func set_unit_stats(value: UnitStats) -> void:
 	_update_ability_visuals(unit_stats)
 
 
-func set_content(value: Array) -> void:
-	content = value
+func set_contents(value: Array) -> void:
+	contents = value
 
-	for lineitem in content:
+	for lineitem in contents:
 		var item_panel_instance := STARTING_ITEM_PANEL_SCENE.instantiate()
-		contents.add_child(item_panel_instance)
+		contents_container.add_child(item_panel_instance)
 
 		if lineitem.item is Item:
 			item_panel_instance.get_node("%Icon").texture = lineitem.item.icon
@@ -50,3 +67,14 @@ func set_content(value: Array) -> void:
 		else:
 			item_panel_instance.get_node("%Icon").texture = GOLD_ICON
 			item_panel_instance.get_node("%ContentLabel").text = "%s Gold"  % str(lineitem.quantity)
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_mouse"):
+		_play_animation("selected")
+		disabled = true
+		panel_selected.emit(unit_stats, contents)
+
+
+func _on_mouse_entered() -> void:
+	_play_animation("hover")
