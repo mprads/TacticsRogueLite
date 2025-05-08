3323457605b9b34a588f4e0ada4ae5f012a42633
Myles Rademaker <mylesprademaker@gmail.com>
2025-05-08 12:55:55 -0700

    Updated engine project settings to reduce bloat by removing 3d portions of engine. Fixed tweening issue of gold ui counting down instead of up on digit overflow

--- scenes/ui/gold_ui.gd
+++ scenes/ui/gold_ui.gd
@@ -6,7 +6,8 @@ class_name GoldUI
 @onready var label: Label = $Label
 
 var tween: Tween
-
+var gold: int = 0
+var fake_count: int = 0 : set = set_fake_count
 
 func _ready() -> void:
 	label.text = "0"
@@ -21,12 +22,18 @@ func set_inventory_manager(value: InventoryManager) -> void:
 	_update_gold()
 
 
+func set_fake_count(value: int) -> void:
+	fake_count = value
+	label.text = str(fake_count)
+
+
 func _update_gold() -> void:
+	gold = inventory_manager.get_gold()
+
 	if tween:
 		tween.kill()
 
 	tween = create_tween()
-
-	tween.tween_property(label, "text", str(inventory_manager.get_gold()), .5)
+	tween.tween_property(self, "fake_count", gold, .5)
 	tween.set_trans(Tween.TRANS_ELASTIC)
 	tween.set_ease(Tween.EASE_IN_OUT)
