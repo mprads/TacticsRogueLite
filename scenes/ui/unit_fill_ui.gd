c64223d4b7ad3c15824de28001a9ff178eeccdb4
Myles Rademaker <mylesprademaker@gmail.com>
2025-05-12 15:29:46 -0700

    Created refill ui and added to run scene

---
+++ scenes/ui/unit_fill_ui.gd
@@ -0,0 +1,31 @@
+extends Control
+class_name UnitFillUI
+
+@export var party_manager: PartyManager : set = set_party_manager
+
+@onready var party_ui: PartyUI = %PartyUI
+@onready var cancel_label: Label = %CancelLabel
+
+
+func _ready() -> void:
+	party_ui.unit_selected.connect(_on_unit_selected)
+
+	var keycode = Utils.get_keycode_from_input_id("cancel")
+	cancel_label.text = "Press [%s] to cancel" % keycode
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("cancel"):
+		visible = false
+
+
+func set_party_manager(value: PartyManager) -> void:
+	if not is_node_ready():
+		await ready
+
+	party_manager = value
+	party_ui.party_manager = party_manager
+
+
+func _on_unit_selected(unit_stats: UnitStats) -> void:
+	pass
