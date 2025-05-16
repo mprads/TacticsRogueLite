f08c3544bc49ffcd7d48db985d47ff3c5f93246d
Myles Rademaker <mylesprademaker@gmail.com>
2025-05-16 14:40:50 -0700

    Added new seed input field on main menu, when populated new run will generate a rng instance with that seed. Added seed and state to run stats

--- resources/run_stats.gd
+++ resources/run_stats.gd
@@ -9,9 +9,13 @@ const STARTING_VIAL_COUNT := 3
 @export var inventory: Dictionary[ItemConfig.KEYS, int]
 @export var artifacts: Array[Artifact]
 @export var party: Array[UnitStats]
-@export var max_party_size := STARTING_PARTY_SIZE
 @export var vials: Array[Vial]
+@export_category("Limits")
+@export var max_party_size := STARTING_PARTY_SIZE
 @export var max_vial_count := STARTING_VIAL_COUNT
+@export_category("Run Data")
+@export var rng_seed: int
+@export var rng_state: int
 
 
 func add_item_to_inventory(key: ItemConfig.KEYS, count: int) -> void:	
