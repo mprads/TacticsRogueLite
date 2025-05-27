6f60383fdcd5ff25f2897cd41e78e93b6f4841b4
Myles Rademaker <mylesprademaker@gmail.com>
2025-05-27 16:54:24 -0700

    Updated enemies to have a ranged attack and melee attack

--- resources/enemies/enemy_stats.gd
+++ resources/enemies/enemy_stats.gd
@@ -11,6 +11,7 @@ class_name EnemyStats
 @export var dimensions: Vector2i
 
 @export_category("Components")
-@export var ability: Ability
+@export var melee_ability: Ability
+@export var ranged_ability: Ability
 @export var ai: EnemyAI
 @export var scene: PackedScene = preload("res://scenes/enemy/enemy.tscn")
