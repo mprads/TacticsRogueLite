279102068bc91e3dfdf64795646086bee9f4f0ae
Myles Rademaker <mylesprademaker@gmail.com>
2025-07-12 16:12:43 -0700

    Fixed naming overlap with parent class

--- resources/abilities/cleanse.gd
+++ resources/abilities/cleanse.gd
@@ -3,5 +3,5 @@ extends Ability
 
 
 func apply_effects(targets: Array[Area2D], _modifier_manager: ModifierManager) -> void:
-	for target in targets:
-		target.status_manager.cleanse_negative_statuses()
+	for entity in targets:
+		entity.status_manager.cleanse_negative_statuses()
