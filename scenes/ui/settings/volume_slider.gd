982a8e13f095927c600d9a42708304f4bfff00e5
Myles Rademaker <mylesprademaker@gmail.com>
2026-01-12 13:17:00 -0800

    Created new volume slider scene for control over each audio bus sound levels. Added sample play to hear adjustements

---
+++ scenes/ui/settings/volume_slider.gd
@@ -0,0 +1,27 @@
+extends VBoxContainer
+
+@export var bus_name: String
+@export var test_sound: SFXConfig.KEYS
+
+@onready var h_slider: HSlider = $HSlider
+@onready var label: Label = $Label
+
+var bus_index: int
+
+func _ready() -> void:
+	h_slider.value_changed.connect(_on_value_changed)
+	bus_index = AudioServer.get_bus_index(bus_name)
+
+	h_slider.value = db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)
+	)
+
+	label.text = "%s Volume" % bus_name
+
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+	if bus_index == 1:
+		SFXPlayer.play(SFXConfig.get_audio_stream(test_sound), true)
+	elif bus_index == 2:
+		MusicPlayer.play(SFXConfig.get_audio_stream(test_sound), true)
