d2314460e4320f66c7bf71b5f9d331578d0d1da6
Myles Rademaker <mylesprademaker@gmail.com>
2026-03-19 15:23:35 -0700

    Create artifact and curse reward pool, added reward resources. 4.6 file updates to existing rewards resources

---
+++ scenes/random_events/chaos_shrine.gd
@@ -0,0 +1,26 @@
+class_name ChaosShrine
+extends Node2D
+
+@export var artifact_manager: ArtifactManager : set = set_artifact_manager
+@export var artifact_pool: WeightedTable
+@export var curse_pool: WeightedTable
+
+@onready var worship_button: Button = %WorshipButton
+@onready var leave_button: Button = %LeaveButton
+
+
+func _ready() -> void:
+	worship_button.pressed.connect(_on_worship_button_pressed)
+	leave_button.pressed.connect(Events.random_event_exited.emit)
+
+
+func set_artifact_manager(value: ArtifactManager) -> void:
+	artifact_manager = value
+
+
+func _on_worship_button_pressed() -> void:
+		var artifact_reward: Artifact = artifact_pool.get_item_in_tier(0).reward_res
+		Events.request_add_artifact.emit(artifact_reward)
+
+		var curse_reward: Artifact = curse_pool.get_item_in_tier(0).reward_res
+		Events.request_add_artifact.emit(curse_reward)
