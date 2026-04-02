30b1eb34036cce477b37a95505448ee37e06ed87
Myles Rademaker <mylesprademaker@gmail.com>
2026-04-01 18:07:14 -0700

    Deleted vial seelect button to make use of exisiting vial panel

--- scenes/random_events/dilapidated_lab.gd
+++ scenes/random_events/dilapidated_lab.gd
@@ -23,12 +23,12 @@ func set_vial_manager(value: VialManager) -> void:
 
 func _generate_option() -> void:
 	var potion: Potion = RNG.array_pick_random(potions)
-	var new_button := VialSelectButton.create_new(potion)
+	var new_button := VialPanel.create_new(potion)
 	vial_container.add_child(new_button)
 	new_button.pressed.connect(_on_button_pressed.bind(new_button))
 
 
-func _on_button_pressed(button: VialSelectButton) -> void:
+func _on_button_pressed(button: VialPanel) -> void:
 	var new_vial := Vial.new()
 	new_vial.potion = button.potion
 	button.queue_free()
