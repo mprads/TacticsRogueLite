ad160c923fde32eb14418df90fcb1d53f189a579
Myles Rademaker <mylesprademaker@gmail.com>
2026-04-17 13:39:29 -0700

    Fixed aspect ratio issue with shop artifacts using artifact icon size of 24x24. Removed upcharge from random events reward pools and moved into if you do not buy anything from the merchant. Updated merchant event to always has increased prices

--- scenes/random_events/merchant.gd
+++ scenes/random_events/merchant.gd
@@ -2,14 +2,23 @@ class_name Merchant
 extends Node2D
 
 @export var shop_artifacts: Array[Artifact]
+@export var no_purchase_curse: Artifact
+@export var option_count := 4
+@export var merchant_upcharge := 1.25
 
 @export var artifact_manager: ArtifactManager : set = set_artifact_manager
 @export var party_manager: PartyManager : set = set_party_manager
 @export var inventory_manager: InventoryManager : set = set_inventory_manager
 
-@onready var available_artifacts := shop_artifacts
+@onready var item_shelf: HBoxContainer = %ItemShelf
+@onready var leave_button: Button = %LeaveButton
 
 var purchase_made := false
+
+
+func _ready() -> void:
+	Events.request_purchase_artifact.connect(_on_request_purchase_artifact)
+	leave_button.pressed.connect(_on_leave_button_pressed)
 
 
 func set_party_manager(value: PartyManager) -> void:
@@ -18,11 +27,7 @@ func set_party_manager(value: PartyManager) -> void:
 
 func set_artifact_manager(value: ArtifactManager) -> void:
 	artifact_manager = value
-	var current_artifacts = artifact_manager.get_artifacts()
-
-	for artifact in current_artifacts:
-		if available_artifacts.has(artifact):
-			available_artifacts.erase(artifact)
+	_generate_shop_artifacts()
 
 
 func set_inventory_manager(value: InventoryManager) -> void:
@@ -33,5 +38,40 @@ func set_inventory_manager(value: InventoryManager) -> void:
 		_on_inventory_gold_changed()
 
 
+func _generate_shop_artifacts() -> void:
+	if not is_node_ready():
+		await ready
+
+	for child in item_shelf.get_children():
+		child.queue_free()
+	
+	var player_artifacts := artifact_manager.get_artifacts()
+	var filtered_artifacts := shop_artifacts.duplicate()
+	for artifact in player_artifacts:
+		filtered_artifacts.erase(artifact)
+
+	for index in clampi(filtered_artifacts.size(), 0, option_count):
+		var selected_artifact = RNG.array_pick_random(filtered_artifacts)
+		var shop_artifact_instance := ShopArtifact.create_new(selected_artifact)
+		item_shelf.add_child(shop_artifact_instance)
+		filtered_artifacts.erase(selected_artifact)
+		shop_artifact_instance.update_gold_cost(merchant_upcharge)
+		shop_artifact_instance.update(inventory_manager.get_gold())
+
+
 func _on_inventory_gold_changed() -> void:
-	pass
+	var player_gold := inventory_manager.get_gold()
+
+	for shop_artifact in item_shelf.get_children():
+		shop_artifact.update(player_gold)
+
+
+func _on_request_purchase_artifact(_artifact: Artifact) -> void:
+	purchase_made = true
+
+
+func _on_leave_button_pressed() -> void:
+	if not purchase_made:
+		Events.request_add_artifact.emit(no_purchase_curse)
+
+	Events.random_event_exited.emit()
